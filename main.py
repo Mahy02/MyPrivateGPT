d643cbcfe32d8d3203b416679b2946b73c613d43
Mahy02 <Mahinouralaa2002@gmail.com>
2023-06-20 15:49:44 +0300

    sec commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#python-dotenv   => to be able to read the .env 
+
+from dotenv import load_dotenv
+import os
+
+
+#this function allows us to read the .env file
+load_dotenv()
+
+print("this comes from .env:", os.getenv('MODEL_TYPE'))
