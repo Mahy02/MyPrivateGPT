8d9bf9b6cbb221f1ab51dcbaef44e4def39f5754
Mahy02 <Mahinouralaa2002@gmail.com>
2023-06-28 11:15:01 +0300

    Added new images

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -24,27 +24,69 @@ css = '''
   padding: 0 1.5rem;
   color: #fff;
 }
+
+.uploader {
+        position: fixed;
+        bottom: 20px;
+        right: 20px;
+    }
+.text-input-container {
+    position: fixed;
+    bottom: 20px;
+    left: 50%;
+    transform: translateX(-50%);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 400px;
+    height: 60px;
+    background-color: #f5f5f5;
+    border-radius: 30px;
+    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+}
+
+.text-input-container input {
+    flex: 1;
+    border: none;
+    outline: none;
+    font-size: 16px;
+    padding: 0 20px;
+    background-color: transparent;
+}
+
+
 '''
 
 
+
+# Text input template
+text_input_template = '''
+<div class="text-input-container">
+    <input type="text" placeholder="Ask a Question about your documents" id="user-question" style="flex: 1; border: none; outline: none; font-size: 16px; padding: 10px;">
+    <button onclick="submitQuestion()" style="margin-left: 10px; padding: 10px 20px; background-color: #4CAF50; color: #fff; border: none; border-radius: 20px; cursor: pointer;">Submit</button>
+</div>
+'''
 #bot template
 bot_template = '''
 <div class="chat-message bot">
     <div class="avatar">
-        <img src="https://i.ibb.co/cN0nmSj/Screenshot-2023-05-28-at-02-37-21.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+        <img src="images/bot.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
     </div>
     <div class="message">{{MSG}}</div>
 </div>
 '''
 
+# https://i.ibb.co/cN0nmSj/Screenshot-2023-05-28-at-02-37-21.png
 
 #user template
 
 user_template = '''
 <div class="chat-message user">
     <div class="avatar">
-        <img src="https://i.ibb.co/rdZC7LZ/Photo-logo-1.png">
+        <img src="images/user.png">
     </div>    
     <div class="message">{{MSG}}</div>
 </div>
 '''
+
+# https://i.ibb.co/rdZC7LZ/Photo-logo-1.png
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #python-dotenv   => to be able to read the .env 
 
-from dotenv import load_dotenv
-import os
+#from dotenv import load_dotenv
+#import os
 
 
 #this function allows us to read the .env file
-load_dotenv()
+#load_dotenv()
 
-print("this comes from .env:", os.getenv('MODEL_TYPE'))
+#print("this comes from .env:", os.getenv('MODEL_TYPE'))
